28a5ed9ba65b2265fef56fcb4490b823caf58d6e
BanTheMan <144498716+BanTheMan@users.noreply.github.com>
2025-06-27 12:04:22 -0500

    Challenge 0

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,15 @@ import glob
 import os
 
 import pandas as pd
+import mlflow
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split 
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -27,13 +28,25 @@ def main(args):
 def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
+    if csv_files := glob.glob(f"{path}/*.csv"):
+        return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+    else:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df: pd.DataFrame):
+    X, y = df[[
+        'Pregnancies',
+        'PlasmaGlucose',
+        'DiastolicBloodPressure',
+        'TricepsThickness',
+        'SerumInsulin',
+        'BMI',
+        'DiabetesPedigree',
+        'Age'
+        ]].values, df['Diabetic'].values
+    
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
@@ -51,11 +64,7 @@ def parse_args():
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.01)
 
-    # parse args
-    args = parser.parse_args()
-
-    # return args
-    return args
+    return parser.parse_args()
 
 # run script
 if __name__ == "__main__":
